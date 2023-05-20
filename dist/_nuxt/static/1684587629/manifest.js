91392767ee84425f2959ac6cf336e64323d8457d
Asgarrrr <caruellej@hotmail.fr>
2023-05-20 16:15:19 +0200

    WOW

--- dist/_nuxt/static/1684587629/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fdonate","\u002Fme\u002Frepos","\u002Fme\u002Fcontact","\u002Fprojects","\u002Fblog","\u002Fprojects\u002Friftmaker","\u002Fprojects\u002Frogue"]})
